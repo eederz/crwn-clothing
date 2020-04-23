092d53f2472e7ca03d1908150a9ef3d38366a173
Eduardo  Baños <ebanos@Eduardos-MacBook-Pro.local>
2020-04-23 10:50:17 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import React from 'react';
 import {HomePage} from './pages/homepage/homepage.component'
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = props => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div >
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
+      </Switch>
+
     </div>
   );
 }
